29fce60cb4f9fee4f6b771622733cd941ced0e98
Romulo Domingues <romulo.domingues@qsdobrasil.com>
2024-04-11 14:52:02 -0300

    Home Page da minha hamburgueria

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ToastContainer } from 'react-toastify'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import AppProvider from './hooks'
+import Routes from './routes/routes'
+import GlobalStyles from './styles/globalStyles'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <Routes />
+    </AppProvider>
+    <ToastContainer autoClose={2000} theme="colored" />
+    <GlobalStyles />
   </React.StrictMode>
-);
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// yarn add eslint -D/
+// yarn eslint --init
+// yarn add eslint-plugin-import-helpers -D
+// yarn add eslint-plugin-react-hooks -D
+// yarn add prettier  eslint-config-prettier eslint-plugin-prettier -D
+// yarn add styled-components
+// yarn add react-hook-form
+// yarn add @hookform/resolvers yup
+// yarn add axios
+// yarn add prop-types
+// yarn add react-toastify
+// yarn add react-router-dom@5
+// yarn add react-elastic-carousel
+// yarn add react-select
+// yarn add @mui/material @emotion/react @emotion/styled
+// yarn add @mui/material @mui/styled-engine-sc styled-components
+// yarn add @mui/x-data-grid
+// yarn add @mui/material
+// yarn add @mui/icons-material
+//
+//
+//
+//
---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const apiCodeBurger = axios.create({
+  // baseURL: 'https://codeburgerapi-production.up.railway.app/'
+
+  baseURL: 'http://localhost:3001' /// CONEXAO COM O MINHA API (BACK-END)
+
+  // baseURL: 'https://burgershop-api.vercel.app/'
+})
+
+apiCodeBurger.interceptors.request.use(async config => {
+  const userData = await localStorage.getItem('codeburger:userData')
+  const token = userData && JSON.parse(userData).token
+  config.headers.Authorization = `Bearer ${token}`
+
+  return config
+})
+
+export default apiCodeBurger
